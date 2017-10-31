2e63707f4758ad50f70186d824c5edc6ddcb49ad
melihgenc <b131210307@sakarya.edu.tr>
2017-10-31 18:55:07 +0300

    Değişiklikler
    Proje başlatıldı ve başlangıç ayarları yapıldı.

---
+++ Fitness/Fitness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fitness.Startup))]
+namespace Fitness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
